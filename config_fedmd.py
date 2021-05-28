275bab736c0f1e76d9f92083cfd21dc799ba7f75
Matthias Schmitt <male.schmitt@posteo.de>
2021-05-28 18:34:04 +0200

    New FedMD config

--- config_fedmd.py
+++ config_fedmd.py
@@ -1,23 +1,27 @@
 from itertools import islice, cycle
-import FedMD
 
 global cfg
 with open('config_base.py') as f:
     exec(f.read())
 
 cfg.update({
-    'samples_per_class': 3,
+    'model_averaging': False,
+    'keep_prev_model': False,
+    'send_global': False,
+    'contrastive_loss': 'none',
+
+    'samples_per_class': 20,
     'dataset': 'CIFAR100',
-    'concentration': 'iid',
+    'concentration': 1,
     'subclasses': [0,2,20,63,71,82],
     'parties': 10,
     'optim': 'Adam',
     'init_public_lr': 0.001,
-    'init_public_epochs': 20,
+    'init_public_epochs': 0,
     'init_public_batch_size': 128,
-    'init_private_epochs': 25,
+    'init_private_epochs': 0,
     'init_private_batch_size': 32,
-    'collab_rounds': 20,
+    'collab_rounds': 100,
     'alignment_mode': 'public',
     'num_alignment': 5000,
     'logits_matching_epochs': 1,
@@ -25,8 +29,10 @@ cfg.update({
     'logits_temperature': 1,
     'private_training_epochs': 4,
     # 'private_training_batchsize': 5, # TODO not supported
-    'model_averaging': False,
+    'upper_bound_epochs': 50,
+    'lower_bound_epochs': 50,
 })
 
+import FedMD
 cfg['model_mapping'] =  list(islice(cycle(range(len(FedMD.FedMD_CIFAR.hyper))),
                                     cfg['parties']))
