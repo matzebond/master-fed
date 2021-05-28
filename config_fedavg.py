301c225dfa3956194ff5a94967a327594ce0747d
Matthias Schmitt <male.schmitt@posteo.de>
2021-05-28 16:59:38 +0200

    New FedAvg config

--- config_fedavg.py
+++ config_fedavg.py
@@ -6,9 +6,13 @@ with open('config_base.py') as f:
 
 cfg.update({
     'model_averaging': True,
-    'samples_per_class': 3,
+    'keep_prev_model': False,
+    'send_global': False,
+    'contrastive_loss': 'none',
+
+    'samples_per_class': 20,
     'dataset': 'CIFAR100',
-    'concentration': 'iid',
+    'concentration': 1,
     'subclasses': [0,2,20,63,71,82],
     'parties': 10,
     'optim': 'Adam',
@@ -17,10 +21,13 @@ cfg.update({
     'init_public_batch_size': 128,
     'init_private_epochs': 0,
     'init_private_batch_size': 32,
-    'collab_rounds': 20,
+    'collab_rounds': 100,
     'alignment_mode': 'none',
+    'logits_matching_epochs': 0,
     'private_training_epochs': 5,
     # 'private_training_batchsize': 5, # TODO not supported
+    'upper_bound_epochs': 50,
+    'lower_bound_epochs': 50,
 })
 
 cfg['model_mapping'] = list(repeat(3, cfg['parties']))
