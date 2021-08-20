Fix KL div

fed.py
@@ -528,13 +528,13 @@ class FedWorker:
             if self.cfg['alignment_target'] == 'rep' \
                 or self.cfg['alignment_target'] == 'both':
                 if self.cfg['alignment_distillation_loss'] == 'KL':
-                    rep = F.log_softmax(rep, dim=-1)
+                    rep = F.softmax(rep, dim=-1)
                 engine.state.rep = torch.cat((engine.state.rep, rep.cpu()), dim=0)
             if self.cfg['alignment_target'] == 'logits' \
                 or self.cfg['alignment_target'] == 'both':
                 logits = logits / self.cfg['alignment_temperature']
                 if self.cfg['alignment_distillation_loss'] == 'KL':
-                    logits = F.log_softmax(logits, dim=-1)
+                    logits = F.softmax(logits, dim=-1)
                 engine.state.logits = torch.cat((engine.state.logits, logits.cpu()), dim=0)
 
         alignment_ds = DataLoader(TensorDataset(alignment_data),

nn.py
@@ -102,8 +102,8 @@ def locality_preserving_loss(local_rep, target_rep, locality_preserving_k=5):
     nbrs = nbrs.fit(target_rep)
     alpha = nbrs.kneighbors_graph(target_rep, mode='distance')
     # g = g.eliminate_zeros()
-    sigma = 100
-    alpha.data = np.exp(-np.power(alpha.data, 2)/sigma)
+    sigma = 10
+    alpha.data = np.exp(-np.power(alpha.data, 2)/(sigma**2))
     alphaT = torch.tensor(alpha.toarray(), device=local_rep.device)
 
     # dists = scidist.squareform(scidist.cdist(local_rep, norm2))